a4b068719ecd02cd9707f145153848abe7c17242
HJTN <82032792+HJTN@users.noreply.github.com>
2026-04-18 19:44:32 +0900

    Title: 달에서 무게 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260418/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int weight = 13;
+        double gravity = 0.165;
+
+        System.out.printf("%d * %f = %.6f\n", weight, gravity, weight * gravity);
+    }
+}
