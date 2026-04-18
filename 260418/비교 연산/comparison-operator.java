53a6ab1785d73970b964594927601211b878a350
HJTN <82032792+HJTN@users.noreply.github.com>
2026-04-18 20:11:07 +0900

    Title: 비교 연산, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260418/비교 연산/comparison-operator.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int A = s.nextInt(), B = s.nextInt();
+        if (A >= B) System.out.println(1);
+        else System.out.println(0);
+        if (A > B) System.out.println(1);
+        else System.out.println(0);
+        if (A <= B) System.out.println(1);
+        else System.out.println(0);
+        if (A < B) System.out.println(1);
+        else System.out.println(0);
+        if (A == B) System.out.println(1);
+        else System.out.println(0);
+        if (A != B) System.out.println(1);
+        else System.out.println(0);
+    }
+}
