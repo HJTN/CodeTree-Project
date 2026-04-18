a8e329c006eed2b9bc94f2ce645979e5b1dec81b
HJTN <82032792+HJTN@users.noreply.github.com>
2026-04-18 20:08:45 +0900

    Title: 음수 구별하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260418/음수 구별하기/separate-negative-number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        System.out.println(N);
+        if (N < 0) System.out.println("minus");
+    }
+}
