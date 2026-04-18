c70127bab01958c8d58f25618351c09ad0c04532
HJTN <82032792+HJTN@users.noreply.github.com>
2026-04-18 19:47:00 +0900

    Title: 점수 입출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260418/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int score = scan.nextInt();
+        System.out.println("Your score is " + score + " point.");
+    }
+}
