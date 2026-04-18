21c0f48d109993f09f61f55f3acafa6c82c3d20a
HJTN <82032792+HJTN@users.noreply.github.com>
2026-04-19 03:36:26 +0900

    Title: B부터 A까지 감소, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260419/B부터 A까지 감소/dec-from-b-to-a.java
+++ 260419/B부터 A까지 감소/dec-from-b-to-a.java
@@ -5,6 +5,6 @@ public class Main {
         Scanner s = new Scanner(System.in);
 
         int A = s.nextInt(), B = s.nextInt();
-        for (int i = B; i > A; i--) System.out.print(i + " ");
+        for (int i = B; i >= A; i--) System.out.print(i + " ");
     }
 }
